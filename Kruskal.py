f17dc39e10400fe2e68893eb0617331ef8884fa4
Yassmin-Ghaly001 <yassminghaly001@gmail.com>
2024-12-25 21:28:45 +0200

    Add files via upload

---
+++ Kruskal.py
@@ -0,0 +1,35 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+def kruskal(n, edges):
+    edges.sort(key=lambda edge: edge[2])  # Sort by weight
+    disjoint_set = DisjointSet(n)
+    mst = []
+
+    for u, v, weight in edges:
+        if disjoint_set.find(u) != disjoint_set.find(v):
+            disjoint_set.union(u, v)
+            mst.append((u, v, weight))
+
+    return mst
+
